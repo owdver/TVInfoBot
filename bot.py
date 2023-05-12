7f1f43acc69cc5148334a3531de94138b7c4e0b1
I'M OWDVER <56756946+owdver@users.noreply.github.com>
2023-05-12 22:24:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ if __name__ == "__main__" :
         root="plugins"
     )
     app = pyrogram.Client(
-        "tmdb bot",
+        "tmdb_bot",
         bot_token=BOT_TOKEN,
         api_id=API_ID,
         api_hash=API_HASH,
