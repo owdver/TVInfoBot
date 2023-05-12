8e63e227249e60ec10b030d683f088acdc125f7a
I'M OWDVER <56756946+owdver@users.noreply.github.com>
2023-05-12 22:59:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,9 +12,10 @@ def is_enabled(value, default):
 
 
 # Set up Pyrogram client
-api_id = os.environ.get("API_ID")
-api_hash = os.environ.get("API_HASH")
-bot_token = os.environ.get("BOT_TOKEN")
+api_id = int(environ['API_ID'])
+api_hash = environ['API_HASH']
+bot_token = environ['BOT_TOKEN']
+
 
 # Set up TMDB API key
 tmdb.API_KEY = os.environ.get("TMDB_API_KEY")
