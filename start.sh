35bf46c6faae1fe67f970ae78341a9ab94d2b36d
I'M OWDVER <56756946+owdver@users.noreply.github.com>
2023-05-29 15:37:29 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-echo "Cloning Repo, Please Wait..."
-git clone -b main https://github.com/owdver/TVInfoBot.git /TVInfoBot
-cd /TVInfoBot
-echo "Installing Requirements..."
-pip3 install -U -r requirements.txt
-echo "Starting Bot, Please Wait..."
-python3 bot.py
